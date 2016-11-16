582b6c702462455c568e7a5743267c0d0e1d4316
Bryce Zuccaro <Bryce_Zuccaro@Zuccaro-Air.local>
2016-11-16 00:55:33 -0700

    Done for 11-15

--- Generators.py
+++ Generators.py
@@ -13,25 +13,25 @@ def alcatel_filter_generator(filter_number, name, acl_vars_array, entry_number):
     entry_number: the amount of entries the user needs.
     """
     #import pdb; pdb.set_trace()
-    with open(name + ".txt", 'w') as acl:
-        alcatel_vars_fixer(name, acl_vars_array, acl)
+    with open(name + ".txt", 'w') as output_file:
+        alcatel_vars_fixer(name, acl_vars_array, output_file)
 
-        acl.write("configure filter ip-filter " + str(filter_number) + " create\n")
-        acl.write("description " + str(name) + "\n")
+        output_file.write("configure filter ip-filter " + str(filter_number) + " create\n")
+        output_file.write("description " + str(name) + "\n")
 
-        entry_generator(acl_vars_array, entry_number, acl)
+        entry_generator(acl_vars_array, entry_number, output_file)
 
-        acl.write("entry 10" + str(entry_number) + " create\n")
-        acl.write("\tmatch\n")
-        acl.write("exit\n")
-        acl.write("action forward\n")
-        acl.write("exit\n\n")
-        acl.write("info\n")
+        output_file.write("entry 10" + str(entry_number) + " create\n")
+        output_file.write("\tmatch\n")
+        output_file.write("exit\n")
+        output_file.write("action forward\n")
+        output_file.write("exit\n\n")
+        output_file.write("info\n")
 
-    with open(name + ".txt", 'r') as acl:
-        print(acl.read())
+    with open(name + ".txt", 'r') as output_file:
+        print(output_file.read())
 
-def alcatel_vars_fixer(name, acl_vars_array, file):
+def alcatel_vars_fixer(name, acl_vars_array, output_file):
     """
     The following loops through our entire array, and calls list_generator.
     Additionally, it checks to see if a list has already been created for any given set of numbers
@@ -62,12 +62,12 @@ def alcatel_vars_fixer(name, acl_vars_array, file):
             #generates ip prefix lists or port lists based on list position.
             if len(acl_vars_array[x][y]) > 1 and y == 3 and check.ip_check(acl_vars_array[x][y]) is True or len(acl_vars_array[x][y]) > 1 and y == 5 and check.ip_check(acl_vars_array[x][y]):
                 existing_list_numbers.append(acl_vars_array[x][y])
-                acl_vars_array[x][y] = list_generator(str(name) + "_ip_list_" + str(ip_list_number), "ip_list", acl_vars_array[x][y], file)
+                acl_vars_array[x][y] = list_generator(str(name) + "_ip_list_" + str(ip_list_number), "ip_list", acl_vars_array[x][y], output_file)
                 existing_list_names.append(str(name) + "_ip_list_" + str(ip_list_number))
                 ip_list_number += 1
             elif len(acl_vars_array[x][y]) > 1 and y == 4 and check.service_check(acl_vars_array[x][y]) is True or len(acl_vars_array[x][y]) > 1 and y == 6 and check.service_check(acl_vars_array[x][y]) is True:
                 existing_list_numbers.append(acl_vars_array[x][y])
-                acl_vars_array[x][y] = list_generator(str(name) + "_port_list_" + str(port_list_number), "port_list", acl_vars_array[x][y], file)
+                acl_vars_array[x][y] = list_generator(str(name) + "_port_list_" + str(port_list_number), "port_list", acl_vars_array[x][y], output_file)
                 existing_list_names.append(str(name) + "_port_list_" + str(port_list_number))
                 port_list_number += 1
             elif duplicate is True:
@@ -76,7 +76,7 @@ def alcatel_vars_fixer(name, acl_vars_array, file):
             else:
                 acl_vars_array[x][y] = acl_vars_array[x][y][0]
 
-def list_generator(name, kind, numbers, file):
+def list_generator(name, kind, numbers, output_file):
     """
     Alcatels lack the capability to state multiple ips or ports in one entry.
     They can use premade lists to specify multiple items in an entry.
@@ -90,19 +90,19 @@ def list_generator(name, kind, numbers, file):
         'port_list' : "port-list "
     }
 
-    file.write("configure filter match-list " + LIST_STRING[kind] + str(name) + " create\n")
+    output_file.write("configure filter match-list " + LIST_STRING[kind] + str(name) + " create\n")
     if kind == "ip_list":
         for ip_addr in numbers:
-            file.write("\tprefix " + str(ip_addr) + "\n")
-        file.write("exit\n")
+            output_file.write("\tprefix " + str(ip_addr) + "\n")
+        output_file.write("exit\n")
         return name
     elif kind == "port_list":
         for port in numbers:
-            file.write("\tport " + str(port) + "\n")
-        file.write("exit\n\n")
+            output_file.write("\tport " + str(port) + "\n")
+        output_file.write("exit\n\n")
         return name
 
-def entry_generator(acl_vars_array, entry_number, file):
+def entry_generator(acl_vars_array, entry_number, output_file):
     """
     Generates entries for each list of variables in acl_vars_array.
 
@@ -116,49 +116,49 @@ def entry_generator(acl_vars_array, entry_number, file):
     i = entry_number
 
     for i in range(0, int(i/10), 1):
-        file.write("entry " + str(acl_vars_array[i][0]) + " create\n")
-        file.write("\tdescription " + str(acl_vars_array[i][1]) + "\n")
+        output_file.write("entry " + str(acl_vars_array[i][0]) + " create\n")
+        output_file.write("\tdescription " + str(acl_vars_array[i][1]) + "\n")
 
         if acl_vars_array[i][2] == "any":
-            file.write("\tmatch protocol *\n")
+            output_file.write("\tmatch protocol *\n")
         else:
-            file.write("\tmatch protocol " + str(acl_vars_array[i][2]) + "\n")
+            output_file.write("\tmatch protocol " + str(acl_vars_array[i][2]) + "\n")
 
         is_title = check.ip_check(str(acl_vars_array[i][3]))
         if is_title is False:
-            file.write("\t\tsrc-ip ip-prefix-list " + str(acl_vars_array[i][3]) + "\n")
+            output_file.write("\t\tsrc-ip ip-prefix-list " + str(acl_vars_array[i][3]) + "\n")
         elif acl_vars_array[i][3] == "any":
             pass
         else:
-            file.write("\t\tsrc-ip " + str(acl_vars_array[i][3]) + "\n")
+            output_file.write("\t\tsrc-ip " + str(acl_vars_array[i][3]) + "\n")
 
         is_title = check.service_check(str(acl_vars_array[i][4]))
         if is_title is False:
-            file.write("\t\tsrc-port port-list " + str(acl_vars_array[i][4]) + "\n")
+            output_file.write("\t\tsrc-port port-list " + str(acl_vars_array[i][4]) + "\n")
         elif acl_vars_array[i][4] == "any":
             pass
         else:
-            file.write("\t\tsrc-port eq " + str(acl_vars_array[i][4]) + "\n")
+            output_file.write("\t\tsrc-port eq " + str(acl_vars_array[i][4]) + "\n")
 
         is_title = check.ip_check(str(acl_vars_array[i][5]))
         if is_title is False:
-            file.write("\t\tdst-ip ip-prefix-list " + str(acl_vars_array[i][5]) + "\n")
+            output_file.write("\t\tdst-ip ip-prefix-list " + str(acl_vars_array[i][5]) + "\n")
         elif acl_vars_array[i][5] == "any":
             pass
         else:
-            file.write("\t\tdst-ip " + str(acl_vars_array[i][5]) + "\n")
+            output_file.write("\t\tdst-ip " + str(acl_vars_array[i][5]) + "\n")
 
         is_title = check.service_check(str(acl_vars_array[i][6]))
         if is_title is False:
-            file.write("\t\tdst-port port-list " + str(acl_vars_array[i][6]) + "\n")
+            output_file.write("\t\tdst-port port-list " + str(acl_vars_array[i][6]) + "\n")
         elif acl_vars_array[i][6] == "any":
             pass
         else:
-            file.write("\t\tdst-port eq " + str(acl_vars_array[i][6]) + "\n")
+            output_file.write("\t\tdst-port eq " + str(acl_vars_array[i][6]) + "\n")
 
-        file.write("\texit\n")
-        file.write("\taction " + str(acl_vars_array[i][7]) + "\n")
-        file.write("exit\n\n")
+        output_file.write("\texit\n")
+        output_file.write("\taction " + str(acl_vars_array[i][7]) + "\n")
+        output_file.write("exit\n\n")
 
 '''def cisco_filter_generator(filter_number, name, acl_vars_array):
     if filter_number <= 99 or filter_number <= 1300 and filter_number >= 1999:'''
